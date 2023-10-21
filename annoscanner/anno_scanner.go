262270ce4b2e7619f0378e935fe3f2de9e10852e
Triwit <triwith.m@ku.th>
2023-10-21 13:45:04 -0700

    Refactor nested loop

--- annoscanner/anno_scanner.go
+++ annoscanner/anno_scanner.go
@@ -27,45 +27,47 @@ func ScanAnnotation(annotation, annotationPattern, filepath string) (Model, erro
 	}
 
 	file, err := os.Open(filepath)
-	if err == nil {
-		scanner := bufio.NewScanner(file)
-		scanner.Split(bufio.ScanLines)
+	if err != nil {
+		return model, err
+	}
 
-		//Loop through each line
-		startOfModel := false
-		lastLineOfModel := StructLastLine
-		for scanner.Scan() {
-			line := scanner.Text()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
 
-			// Start extracting model's attribute
-			if startOfModel {
-				// Skip the struct declaration line and all comments
-				if commentPattern.MatchString(line) {
-					continue
-				}
+	//Loop through each line
+	startOfModel := false
+	lastLineOfModel := StructLastLine
+	for scanner.Scan() {
+		line := scanner.Text()
 
-				// Extract model name from the first line
-				if structPattern.MatchString(line) {
-					structDC := strings.Fields(strings.TrimSpace(line))
-					model.Name = structDC[1]
-					continue
-				}
-
-				// Stop finding attribute when reaching the end of struct
-				if strings.TrimSpace(line) == lastLineOfModel {
-					return model, nil
-				}
-
-				// Extract model's attribute to this sample format {name:username, dataType:string}
-				attribute := strings.Fields(strings.TrimSpace(line))
-				model.Attributes[attribute[0]] = MakeModelAttribute(attribute[0], attribute[1])
+		// Start extracting model's attribute
+		if startOfModel {
+			// Skip the struct declaration line and all comments
+			if commentPattern.MatchString(line) {
+				continue
 			}
 
-			// Toggle startOfModel when it detects annotation
-			if !startOfModel && strings.Contains(line, "//") && strings.Contains(line, annotation) {
-				model.Annotation = regex.FindString(line)
-				startOfModel = true
+			// Extract model name from the first line
+			if structPattern.MatchString(line) {
+				structDC := strings.Fields(strings.TrimSpace(line))
+				model.Name = structDC[1]
+				continue
 			}
+
+			// Stop finding attribute when reaching the end of struct
+			if strings.TrimSpace(line) == lastLineOfModel {
+				return model, nil
+			}
+
+			// Extract model's attribute to this sample format {name:username, dataType:string}
+			attribute := strings.Fields(strings.TrimSpace(line))
+			model.Attributes[attribute[0]] = MakeModelAttribute(attribute[0], attribute[1])
+		}
+
+		// Toggle startOfModel when it detects annotation
+		if !startOfModel && strings.Contains(line, "//") && strings.Contains(line, annotation) {
+			model.Annotation = regex.FindString(line)
+			startOfModel = true
 		}
 	}
 
